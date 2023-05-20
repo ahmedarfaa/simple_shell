33ee5b9a5509bb68d63aa29a78502297700feccb
ahmedarfaa <ahmedarfaa594@gmail.com>
2023-05-20 12:22:13 -0700

    memory leek 3

--- main.c
+++ main.c
@@ -34,7 +34,7 @@ int main(int argc, char **argv, char **env)
 		write(STDOUT_FILENO, "$ ", 2);
 		read = getline(&input, &input_size, stdin);
 		if (read == -1)
-			exit(1);
+			free(input), exit(1);
 		if (input[read - 1] == '\n')
 			input[read - 1] = '\0';
 		if (input[0] == '\0')
