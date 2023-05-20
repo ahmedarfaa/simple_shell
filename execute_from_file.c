ad747c6bc87a0609b006115d66d3f74e8dbae246
Muffinz1 <amiryou120@gmail.com>
2023-05-20 08:33:06 -0700

    fixing betty

--- execute_from_file.c
+++ execute_from_file.c
@@ -43,7 +43,7 @@ void execute_commands_from_file(char *filename, char **env)
 		if (execve(fullpath, args, env) == -1)
 		{
 		_sprintf(error_message, "%s: %d: %s: not found\n", args[0], count, args[0]);
-		write(STDERR_FILENO, error_message, strlen(error_message));
+		write(STDERR_FILENO, error_message, _strlen(error_message));
 		exit(127);
 		}
 	}
